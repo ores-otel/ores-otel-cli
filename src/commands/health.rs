cbcfc05a32227f1e978ec2152b6eedf4182f0229
Alexander Mills <alex@oresoftware.com>
2026-09-14 18:25:48 -0500

    feat: color health output and honor debug threshold

--- src/commands/health.rs
+++ src/commands/health.rs
@@ -1,6 +1,7 @@
 #![forbid(unsafe_code)]
 
 use next_loggers::Logger;
+use ores_clis_core::{ColorRole, LogLevel, paint};
 use serde_json::json;
 
 use crate::config::Config;
@@ -16,13 +17,19 @@ pub fn run(config: &Config, log: &Logger) -> Result<(), CliError> {
     if config.json {
         println!("{body}");
     } else {
-        println!("ok {}", config.api_base);
+        println!(
+            "{} {}",
+            paint(config.runtime.color_stdout(), ColorRole::Success, "ok"),
+            paint(config.runtime.color_stdout(), ColorRole::Info, &config.api_base)
+        );
     }
-    let _ = log
-        .debug(vec![json!("health command completed")])
-        .add_fields(telemetry::output_fields(config.json))
-        .add_trace("ores-trace-L-LxAlnEnDJDjusvVcXla", false)
-        .add_routine_id(ROUTINE_ID)
-        .send();
+    if config.runtime.allows_log(LogLevel::Debug) {
+        let _ = log
+            .debug(vec![json!("health command completed")])
+            .add_fields(telemetry::output_fields(config.json))
+            .add_trace("ores-trace-L-LxAlnEnDJDjusvVcXla", false)
+            .add_routine_id(ROUTINE_ID)
+            .send();
+    }
     Ok(())
 }
